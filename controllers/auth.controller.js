f7a66ed5791059660b6933bd76653276f3b34511
KagMin <phankangmin@gmail.com>
2025-06-19 20:32:58 +0700

    update structor

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,15 +11,15 @@ exports.register = async (req, res) => {
 
     const userExists = await User.findOne({ email });
     if (userExists)
-      return res.status(400).json({ message: "Email đã tồn tại" });
+      return res.status(400).json({ message: "Email already exist!" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({ name, email, password: hashedPassword });
 
-    res.status(201).json({ message: "Đăng ký thành công", user });
+    res.status(201).json({ message: "Register Successfully!", user });
   } catch (error) {
-    res.status(500).json({ message: "Lỗi server", error });
+    res.status(500).json({ message: "Lỗi server!", error });
   }
 };
 
@@ -29,16 +29,16 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Email do not exist" });
+    if (!user) return res.status(400).json({ message: "Email do not exist!" });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ message: "Wrong Password" });
+      return res.status(400).json({ message: "Wrong Password!" });
 
     const token = jwt.sign({ id: user._id }, SECRET, { expiresIn: "1d" });
 
-    res.json({ message: "Login Successfully", token, user });
+    res.json({ message: "Login Successfully!", token, user });
   } catch (error) {
-    res.status(500).json({ message: "Error server", error });
+    res.status(500).json({ message: "Error server!", error });
   }
 };
